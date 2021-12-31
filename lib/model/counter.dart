bcc88ff57daf93f22331c9ebbe6acdbb09404fb3
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-12-31 17:28:35 +0530

    first

---
+++ lib/model/counter.dart
@@ -0,0 +1,10 @@
+import 'package:scoped_model/scoped_model.dart';
+
+class Counter extends Model {
+  int _counter = 0;
+  int get counter => _counter;
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+}
