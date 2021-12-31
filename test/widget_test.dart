f1f1f2daff5f24ef3cadb2659c444ca7a239df62
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-12-31 12:18:06 +0530

    first to first

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:provider_et_sqflite/main.dart';
+import 'package:provider_et_sqflite/view/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
