ca72c280c163f32860d01019966004bf7a139d77
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-01 07:03:17 +0530

    this is final again

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider_et_sqflite/model/counter.dart';
 import 'package:provider_et_sqflite/view/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(
+      counter: Counter(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
